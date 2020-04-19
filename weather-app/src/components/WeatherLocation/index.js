23054fe541b6af0cb84def67bc509b6b017b2e18
Wilfrido Bonett <wbonett10@gmail.com>
2020-04-19 14:21:25 -0500

    I add handle update data

--- weather-app/src/components/WeatherLocation/index.js
+++ weather-app/src/components/WeatherLocation/index.js
@@ -2,23 +2,44 @@ import React, { Component } from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import {
-    SUN
+    SUN, CLOUD
 } from './../../constants/weather';
 import './styles.css';
 
-const data = {
-    temperature: 10,
-    weatherState: SUN,
-    humidity: 10,
-    wind: '10 m/s'
-};
-
 class WeatherLocation extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            city: 'Alemania',
+            data: {
+                temperature: 10,
+                weatherState: SUN,
+                humidity: 10,
+                wind: '10 m/s'
+            }
+        }
+    }
+
+    hanleUpdateClick = () => {
+        this.setState({
+            city: 'Colombia',
+            data: {
+                temperature: 30,
+                weatherState: CLOUD,
+                humidity: 30,
+                wind: '20 m/s'
+            }
+        })
+    }
+
     render() {
+        const { city, data } = this.state;
         return (
             <div className="weather_location_content">
-                <Location city={"Alemania"} />
+                <Location city={city} />
                 <WeatherData data={data} />
+                <button onClick={this.hanleUpdateClick}>Actualizar</button>
             </div>
         )
     }
