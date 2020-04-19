6036cc5c82457d4b2f1972aa93101226b8542a08
Wilfrido Bonett <wbonett10@gmail.com>
2020-04-19 18:42:37 -0500

    I Add select location handle

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import LocationList from './components/LocationList';
 import './App.css';
+
+const theme = createMuiTheme({
+  pallete: {
+     primary: {
+      "50": "#21412a",
+      "100": "#21412a",
+      "200": "#21412a",
+      "300": "#21412a",
+      "400": "#21412a",
+      "500": "#21412a",
+      "600": "#21412a",
+      "700": "#21412a",
+      "800": "#21412a",
+      "900": "#21412a",
+      "A100": "#21412a",
+      "A200": "#21412a",
+      "A400": "#21412a",
+      "A700": "#21412a",
+      "contrastDefaultColor": "light"
+    }
+
+  },
+});
 
 const cities = [
   "Medellin,col",
@@ -11,12 +35,21 @@ const cities = [
   "Madrid,es",
 ];
 
-function App() {
-  return (
-    <div className="App">
-      <LocationList cities={cities} />
-    </div>
-  );
+class App extends Component {
+
+  handleSelectedLocation = city => {
+    console.log('asdasd');
+  }
+
+  render() {
+    return (
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <LocationList cities={cities} onSelectedLocation={this.handleSelectedLocation} />
+        </div>
+      </MuiThemeProvider>
+    );
+  }
 }
 
 export default App;
--- weather-app/src/components/LocationList.js
+++ weather-app/src/components/LocationList.js
@@ -2,18 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import WeatherLocation from './WeatherLocation';
 
-const strToComponents = cities => (
-    cities.map((city) => <WeatherLocation city={city} /> )    
-);
+const LocationList = ({ cities, onSelectedLocation }) => {
+    
+    const HandleWeatherLocationClick = city => {
+        onSelectedLocation(city);
+    }
 
-const LocationList = ({ cities }) => (
-    <div>
-        { strToComponents(cities) }
-    </div>
-);
+    const strToComponents = cities => (
+        cities.map(city => ( <WeatherLocation city={city} key={city} onWeatherLocationClick={() => HandleWeatherLocationClick(city)} />) )
+    );
+    
+    return (
+        <div>
+            {strToComponents(cities)}
+        </div>
+    );
+}
 
 LocationList.propTypes = {
     cities: PropTypes.array.isRequired,
+    onSelectedLocation: PropTypes.func,
 }
 
 export default LocationList;
